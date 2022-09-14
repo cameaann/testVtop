e792b44295e7f480c6025596361e82bc4dbee60e
Anna <cameaann@gmail.com>
2022-09-14 15:06:49 +0300

    added methods to selection

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import * as flsFunctions from './modules/functions.js';
-import './modules/validation.js';
+import './modules/form.js';
 import './modules/animation.js';
 import Swiper, { Navigation, Pagination } from "swiper";
 
--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -35,6 +35,254 @@ var successMessage = document.querySelector('span[name="success-message"]');
 var passHintMessage = document.querySelector('.validation');
 var passHintItems = [passLength, lowerLetter, upperLetter, symbol, digit];
 
+
+function setNationalities(){
+    var nationalities = [
+        'Afghan',
+        'Albanian',
+        'Algerian',
+        'American',
+        'Andorran',
+        'Angolan',
+        'Antiguans',
+        'Argentinean',
+        'Armenian',
+        'Australian',
+        'Austrian',
+        'Azerbaijani',
+        'Bahamian',
+        'Bahraini',
+        'Bangladeshi',
+        'Barbadian',
+        'Barbudans',
+        'Batswana',
+        'Belarusian',
+        'Belgian',
+        'Belizean',
+        'Beninese',
+        'Bhutanese',
+        'Bolivian',
+        'Bosnian',
+        'Brazilian',
+        'British',
+        'Bruneian',
+        'Bulgarian',
+        'Burkinabe',
+        'Burmese',
+        'Burundian',
+        'Cambodian',
+        'Cameroonian',
+        'Canadian',
+        'Cape Verdean',
+        'Central African',
+        'Chadian',
+        'Chilean',
+        'Chinese',
+        'Colombian',
+        'Comoran',
+        'Congolese',
+        'Costa Rican',
+        'Croatian',
+        'Cuban',
+        'Cypriot',
+        'Czech',
+        'Danish',
+        'Djibouti',
+        'Dominican',
+        'Dutch',
+        'East Timorese',
+        'Ecuadorean',
+        'Egyptian',
+        'Emirian',
+        'Equatorial Guinean',
+        'Eritrean',
+        'Estonian',
+        'Ethiopian',
+        'Fijian',
+        'Filipino',
+        'Finnish',
+        'French',
+        'Gabonese',
+        'Gambian',
+        'Georgian',
+        'German',
+        'Ghanaian',
+        'Greek',
+        'Grenadian',
+        'Guatemalan',
+        'Guinea-Bissauan',
+        'Guinean',
+        'Guyanese',
+        'Haitian',
+        'Herzegovinian',
+        'Honduran',
+        'Hungarian',
+        'I-Kiribati',
+        'Icelander',
+        'Indian',
+        'Indonesian',
+        'Iranian',
+        'Iraqi',
+        'Irish',
+        'Israeli',
+        'Italian',
+        'Ivorian',
+        'Jamaican',
+        'Japanese',
+        'Jordanian',
+        'Kazakhstani',
+        'Kenyan',
+        'Kittian and Nevisian',
+        'Kuwaiti',
+        'Kyrgyz',
+        'Laotian',
+        'Latvian',
+        'Lebanese',
+        'Liberian',
+        'Libyan',
+        'Liechtensteiner',
+        'Lithuanian',
+        'Luxembourger',
+        'Macedonian',
+        'Malagasy',
+        'Malawian',
+        'Malaysian',
+        'Maldivan',
+        'Malian',
+        'Maltese',
+        'Marshallese',
+        'Mauritanian',
+        'Mauritian',
+        'Mexican',
+        'Micronesian',
+        'Moldovan',
+        'Monacan',
+        'Mongolian',
+        'Moroccan',
+        'Mosotho',
+        'Motswana',
+        'Mozambican',
+        'Namibian',
+        'Nauruan',
+        'Nepalese',
+        'New Zealander',
+        'Nicaraguan',
+        'Nigerian',
+        'Nigerien',
+        'North Korean',
+        'Northern Irish',
+        'Norwegian',
+        'Omani',
+        'Pakistani',
+        'Palauan',
+        'Panamanian',
+        'Papua New Guinean',
+        'Paraguayan',
+        'Peruvian',
+        'Polish',
+        'Portuguese',
+        'Qatari',
+        'Romanian',
+        'Russian',
+        'Rwandan',
+        'Saint Lucian',
+        'Salvadoran',
+        'Samoan',
+        'San Marinese',
+        'Sao Tomean',
+        'Saudi',
+        'Scottish',
+        'Senegalese',
+        'Serbian',
+        'Seychellois',
+        'Sierra Leonean',
+        'Singaporean',
+        'Slovakian',
+        'Slovenian',
+        'Solomon Islander',
+        'Somali',
+        'South African',
+        'South Korean',
+        'Spanish',
+        'Sri Lankan',
+        'Sudanese',
+        'Surinamer',
+        'Swazi',
+        'Swedish',
+        'Swiss',
+        'Syrian',
+        'Taiwanese',
+        'Tajik',
+        'Tanzanian',
+        'Thai',
+        'Togolese',
+        'Tongan',
+        'Trinidadian/Tobagonian',
+        'Tunisian',
+        'Turkish',
+        'Tuvaluan',
+        'Ugandan',
+        'Ukrainian',
+        'Uruguayan',
+        'Uzbekistani',
+        'Venezuelan',
+        'Vietnamese',
+        'Welsh',
+        'Yemenite',
+        'Zambian',
+        'Zimbabwean'];
+            for(var i=0; i<nationalities.length; i++){
+                var option = document.createElement("option");
+                option.value = nationalities[i];
+                option.innerHTML = option.value;
+                nationality.appendChild(option);
+            }
+}
+function setMonths(){
+    var months = ['January', 'Febrary', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'];
+    for(var i=0; i<months.length; i++){
+        var option = document.createElement("option");
+        option.value = months[i];
+        option.innerHTML = option.value;
+        monthbirth.appendChild(option);
+    }
+}
+function setDays(){
+    for(var i=1; i<32; i++){
+        var option = document.createElement("option");
+        option.value = i;
+        option.innerHTML = option.value;
+        daybirth.appendChild(option);
+    }
+}
+
+function generateYears(){
+    var years = [];
+    const maxYear = new Date().getFullYear()-14;
+    const minYear = maxYear-80;
+    for (var i = maxYear; i >= minYear; i--) {
+        years.push(i)
+      }
+
+    for(var i=0; i<years.length; i++){
+        var option = document.createElement("option");
+        option.value = years[i];
+        option.innerHTML = option.value;
+        yearbirth.appendChild(option);
+    }
+
+
+}
+
+setNationalities();
+setDays();
+setMonths();
+generateYears();
+
+
+//VALIDATION
+//-----------------------------------------------------------------
 const isValidInput = (inp) => {
     var check = inp.value.trim().length !== 0;
     if (check) { inp.previousElementSibling.classList.remove("hidden") }
@@ -44,8 +292,7 @@ const isValidInput = (inp) => {
 
 
 const isValidEmail = (email) => {
-    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
+    const reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
     if (reg.test(String(email).toLocaleLowerCase())) {
         emailInput.previousElementSibling.classList.remove("hidden");
@@ -243,6 +490,9 @@ passInput.addEventListener("blur", () => {
     passHintMessage.classList.add('hidden');
     if (!isFormValid) { invalidateElm(passInput) };
 });
+
+
+//SENDING DATA TO SERVER
 
 function sendData(userData) {
     // var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
